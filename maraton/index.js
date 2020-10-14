295819562c1f19a0e746c81274d381189a9bb972
lgLuis <luis_unlz@yahoo.com.ar>
2020-10-14 11:47:15 -0300

    pendiente parte final ej 10

--- maraton/index.js
+++ maraton/index.js
@@ -1,11 +1,14 @@
 /*
 Bienvenido/a!!
-Te proponemos 10 ejercicios de JS para resolver de forma individual. Si te trabas con alguno, podes continuar con otro de forma independiente. Si corresponde el ejercicio tiene un <div> asignado en el HTML para visualizar el resultado. Algunos te piden solo tocar JS otros, JS + HTML.
+Te proponemos 10 ejercicios de JS para resolver de forma individual. Si te trabas con alguno, 
+podes continuar con otro de forma independiente. 
+Si corresponde el ejercicio tiene un <div> asignado en el HTML para visualizar el resultado. 
+Algunos te piden solo tocar JS otros, JS + HTML.
 Exitos! Happy coding! - Guayerd
 
 ------
-----------         RELIZADOS          01 02 03 04 05 06 08 09
--------------      INCOMPLETOS        07 10
+----------         RELIZADOS          01 02 03 04 05 06 07 08 09
+-------------      INCOMPLETOS        10
 ----------         SIN COMENZAR        
 ------
 
@@ -44,7 +47,7 @@ function blurHandler(e){
 Ej - 3: Codear la función mostrarSaludo() para mostrar un saludo desde el div.ej3
 */
 
-const ej3Div = document.querySelector(".ej3-resultado");
+const ej3Div          =document.querySelector(".ej3-resultado");
 ej3Div.addEventListener("click", mostrarSaludo);
 
 function mostrarSaludo(){
@@ -112,6 +115,7 @@ function sumatoria(){
 }
 */
 function sumatoria(){
+  console.log("Si, estoy ehecutando la func del ej. 5to")//debug
   if(isNaN(inp1.value)||isNaN(inp2.value)){
     divRdo.innerText    ="INGRESO ERRÓNEO"
   }else{
@@ -119,8 +123,6 @@ function sumatoria(){
     divRdo.innerText    =`El rdo es la suma es ${rdo}`
   }
 }
-
-
 
 /*
 Ej - 6: Completar los elementos <select> del div.ej6 con los arrays correspondientes.
@@ -157,18 +159,19 @@ let sel             =document.querySelector(".momentos");  ////sel.value
 
 momento.forEach(function(instante){
   let option        =document.createElement("option");
-  option.innerHTML  =instante;
   option.value      =instante;
+  option.innerHTML  =instante;
   sel.appendChild(option);
 });
 
-let rdo             =document.querySelector(".ej7-resultado");
+sel.addEventListener("change", rdoHandler);
 
-
-///   ??????????????????????????????
-
-
-
+function rdoHandler(){
+  alert("holaaaaaaaa es el ej7");// DEBUG, para verif si se ejecuta la func.
+  const rdo			=document.querySelector(".ej7-resultado");
+  rdo.innerText		=sel.value;
+  console.log( "aqui ej 7");
+}
 
 
 /*
@@ -234,6 +237,8 @@ let divContenedor     =document.querySelector(".ej9-resultado");
 let juntarUsuarios="";
 
 usuarios.forEach(function(usuario){
+  alert(usuario.nombre);// lo utilizo para verif que nombre llega
+//  concateno dentro de la memoria
   juntarUsuarios+=`
   <div class="usuario-contenedor">
   <div class="fila1">
@@ -247,8 +252,7 @@ usuarios.forEach(function(usuario){
 </div>
   `
 });
-divContenedor.innerHTML=juntarUsuarios;
-
+divContenedor.innerHTML=juntarUsuarios; //asigno al final, mejora la performance.
 
 /*
 Ej - 10: Nuestro cliente es una empresa de marketing que busca generar
@@ -263,11 +267,11 @@ Debemos crear para ellos un formulario de carga con los siguientes datos:
   - Botón "Guardar"
   - Botón "Finalizar"
 
-  a) Crear el <form> dentro del elemento div.ej10-form
-  b) Crear los elementos input, select, button que correspondan
-  c) Cuando el usuario hace "click" en el botón, guardar los datos de contacto en un array
-  d) Limpiar los elementos del form
-  e) Cada vez que se guarden datos, guardarlos tambien en localStorage
+  a)ok  Crear el <form> dentro del elemento div.ej10-form
+  b)ok  Crear los elementos input, select, button que correspondan
+  c)ok Cuando el usuario hace "click" en el botón, guardar los datos de contacto en un array
+  d)ok Limpiar los elementos del form
+  e)ok Cada vez que se guarden datos, guardarlos tambien en localStorage
   f) Cuando la página se vuelve a cargar, recuperar el array desde localStorage si es que existe.
   e) Cuando se hace click en "Finalizar", mostrar de forma clara el listado de contactos y sus datos dentro de div.ej10-resultado y limpiar el localStorage.
 
@@ -282,6 +286,112 @@ Debemos crear para ellos un formulario de carga con los siguientes datos:
   Constancia: RAE
   Firmeza y perseverancia del ánimo en las resoluciones y en los propósitos.
  */
+
+let divParaForm          =document.querySelector(".ej10-form");
+let form                 =document.createElement("form");
+let rdofinal             =document.querySelector(".ej10-resultado");
+
+let inputNombre          =document.createElement("input");
+inputNombre.type         ="text";
+inputNombre.placeholder  ="NOMBRE..."
+
+let inputTel             =document.createElement("input");
+inputTel.type            ="number";
+inputTel.placeholder     ="TELEFONO";
+
+let inputMail            =document.createElement("input");
+inputMail.placeholder    ="MAIL";
+
+let selectHorario        =document.createElement("select");
+let optH01               =document.createElement("option");
+let optH02               =document.createElement("option");
+let optH03               =document.createElement("option");
+optH01.innerText         ="MAÑANA";
+optH02.innerText         ="TARDE";
+optH03.innerText         ="NOCHE";
+selectHorario.appendChild(optH01);
+selectHorario.appendChild(optH02);
+selectHorario.appendChild(optH03);
+
+let selectProduc        =document.createElement("select");
+let optP01              =document.createElement("option");
+let optP02              =document.createElement("option");
+let optP03              =document.createElement("option");
+optP01.innerText        ="nnn01";
+optP02.innerText        ="nnn02";
+optP03.innerText        ="nnn03";
+selectProduc.appendChild(optP01);
+selectProduc.appendChild(optP02);
+selectProduc.appendChild(optP03);
+
+let botonSave           =document.createElement("button");
+botonSave.innerText     ="GUARDAR";
+botonSave.type          ="submit";// no sé si es necesario el SUBMIT..
+botonSave.classList.add ="unClassAqui"// como se agraga una clase o id
+
+let botonFinish         =document.createElement("button"); //.ej10 button
+botonFinish.innerText   ="FINALIZAR";
+
+form.appendChild(inputNombre);
+form.appendChild(inputTel);
+form.appendChild(inputMail);
+form.appendChild(selectHorario);
+form.appendChild(selectProduc);
+form.appendChild(botonSave);
+form.appendChild(botonFinish);
+
+divParaForm.appendChild(form);
+
+
+const contactos=[];
+
+//if(localStorage.getItem("arrayUsuarios")){
+//  contactos=JSON.parse(localStorage.getItem("contactos"))
+//};
+
+//boton: nunca realizara un submit
+form.addEventListener("submit", btnSaveHandler);
+//botonFinish.addEventListener("click", btnFinishHandler);
+function btnSaveHandler(e){
+  e.preventDefault();// sino se recarga la pág  y tengo que detenerlo
+  console.log("funcion del ejercicio 10");
+  const usuario ={
+    nombre:   inputNombre.value,
+    telefono: inputTel.value,
+    mail:     inputMail.value
+  }
+  contactos.push(usuario);
+  localStorage.setItem("arrayUsuarios", JSON.stringify(contactos));
+  form.reset();
+}
+
+form.addEventListener("click",btnFinish );
+
+// con ayuda 
+function btnFinish(e){
+  e.preventDefault();
+  contactos.forEach(function(contacto, index){
+    let ul        =createElement("ul");
+    ul.innerText  =`CLIENTE: ${index+1}`
+    let liName    =createElement("li");
+    il.innerText  =`NOMBRE: ${contacto.nombre}`
+
+    ul.appendChild(liName)
+    rdofinal.appendChild(ul);
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 // 01-COMPRENDIENDO LO BÁSICO.
 let divParaForm         =document.querySelector(".ej10-form");
@@ -359,79 +469,3 @@ divParaForm.appendChild(form);
 divParaForm.appendChild(selectHorario);
 divParaForm.appendChild(selectProduc);
 */
-
-let divParaForm          =document.querySelector(".ej10-form");
-let form                 =document.createElement("form");
-
-let inputNombre          =document.createElement("input");
-inputNombre.type         ="text";
-inputNombre.placeholder  ="NOMBRE..."
-
-let inputTel             =document.createElement("input");
-inputTel.type            ="number";
-inputTel.placeholder     ="TELEFONO";
-
-let inputMail            =document.createElement("input");
-inputMail.placeholder    ="MAIL";
-
-let selectHorario        =document.createElement("select");
-let optH01               =document.createElement("option");
-let optH02               =document.createElement("option");
-let optH03               =document.createElement("option");
-optH01.innerText         ="MAÑANA";
-optH02.innerText         ="TARDE";
-optH03.innerText         ="NOCHE";
-selectHorario.appendChild(optH01);
-selectHorario.appendChild(optH02);
-selectHorario.appendChild(optH03);
-
-let selectProduc        =document.createElement("select");
-let optP01              =document.createElement("option");
-let optP02              =document.createElement("option");
-let optP03              =document.createElement("option");
-optP01.innerText        ="XBOX";
-optP02.innerText        ="PS4";
-optP03.innerText        ="NINTENDO SWITCH";
-selectProduc.appendChild(optP01);
-selectProduc.appendChild(optP02);
-selectProduc.appendChild(optP03);
-
-let botonSave           =document.createElement("button");
-botonSave.innerText     ="GUARDAR";
-botonSave.type          ="submit";
-
-let botonFinish         =document.createElement("button");
-botonFinish.innerText   ="FINALIZAR";
-
-
-form.appendChild(inputNombre);
-form.appendChild(inputTel);
-form.appendChild(inputMail);
-form.appendChild(selectHorario);
-form.appendChild(selectProduc);
-form.appendChild(botonSave);
-form.appendChild(botonFinish);
-
-divParaForm.appendChild(form);
-
-let contactos=[];
-if(localStorage.getItem("contactos")){
-  contactos             =JSON.parse(localStorage.getItem(contactos))
-};
-
-botonSave.addEventListener("click", btnSaveHandler);
-botonFinish.addEventListener("click", btnFiishHandler);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
